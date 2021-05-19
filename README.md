fa3181b86102cce6d7bb66ccec0a30baadc9e7de
shantel williams <walterdtrt@aol.com>
2021-05-19 15:12:17 -0400

    Second commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# UnderPressure Project
+
+This is the most recent release of my game Under Pressure. This game was a project for my intro to game programming course.
+This is a text base game with mouse click. It has multple choice answers and only 2 outcomes. I used the pygame Library
+and gimp for simple graphics and transitions. This was my first coding project. I decided to go in and use what I learned
+to clean up alot of the code. This is still a work in prograss as I continue to clean up code and fixes. Thank you!
--- UnderPressure3FinalTouches.py
+++ UnderPressure3FinalTouches.py
@@ -1,12 +1,12 @@
 #########################################################################################################################################
-##Project name: UnderPressureBeta1.1
+##Project name: UnderPressureBeta
 ##Author: Shantel Willams
-##Date: 09/25/2018
+##Date: 05/19/2021
 ##
-## This is the last beta release of my game Under Pressure. This game was a project for my intro to game programming course.
-## This is a text base game witH mouse click. It has multple choice answers and only 2 outcomes. I used the pygame Library
+## This is the most recent release of my game Under Pressure. This game was a project for my intro to game programming course.
+## This is a text base game with mouse click. It has multple choice answers and only 2 outcomes. I used the pygame Library
 ## and gimp for simple graphics and transitions. This was my first coding project. I decided to go in and use what I learned
-## to clean up alot of the code. Thank you!
+## to clean up alot of the code. This is still a work in prograss as I continue to clean up code and fixes. Thank you!
 ###########################################################################################################################################
 
 
